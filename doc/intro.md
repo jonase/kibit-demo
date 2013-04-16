91f3f90704bae92f4d976e4f1c40721835f92f7e
Jonas Enlund <jonas.enlund@gmail.com>
2013-04-16 18:27:20 +0300

    gh-pages

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to kibit-demo
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/kibit_demo/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kibit-demo.core-test
-  (:use clojure.test
-        kibit-demo.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
